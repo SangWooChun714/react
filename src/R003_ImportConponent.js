1886b12196088eea0e666e3b2a0b40cf9c932afd
SangWoo Cheon <mig55@naver.com>
2022-01-27 15:40:29 +0900

    react

---
+++ src/R003_ImportConponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class r003_importComponent extends Component {
+    render () {
+        return (
+            <h2>[This is imported component]</h2>
+        )
+    }
+}
+
+export default r003_importComponent;
